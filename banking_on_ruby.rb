792b67159404d805201bc574f3e678e714e5bde9
grenzk <74033991+grenzk@users.noreply.github.com>
2022-03-23 14:18:00 +0800

    Add withdraw method

--- banking_on_ruby.rb
+++ banking_on_ruby.rb
@@ -17,6 +17,15 @@ class Account
     end
   end
 
+  def withdraw(pin_number, amount)
+    if pin == pin_number
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{@balance}."
+    else
+      puts pin_error
+    end
+  end
+
   private
 
   def pin
